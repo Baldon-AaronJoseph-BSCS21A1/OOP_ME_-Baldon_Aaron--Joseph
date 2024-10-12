2faa19b6957c213ec84c80810bc24f71387fac6f
Baldon-AaronJoseph-BSCS21A1 <aaronjosephtbaldon062505@gmail.com>
2024-10-12 11:59:32 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+// See https://aka.ms/new-console-template for more information
+
+
+var name = "Aaron Joseph Baldon";
+int score = Examination.Start();
+
+Console.WriteLine($"Student: {name}\r\nScore: {score}/20");
+
